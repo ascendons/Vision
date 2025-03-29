2057c72094627bc2c155c3a3e8d6f947aa160fc4
pankajThakur1874 <pankajthakur1874@gmail.com>
2025-03-30 02:40:19 +0530

    Initial commit

--- ultron/src/main/java/com/vision/ultron/MainRunner.java
+++ ultron/src/main/java/com/vision/ultron/MainRunner.java
@@ -1,13 +1,13 @@
-package com.vision.backend;
+package com.vision.ultron;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class MainRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(MainRunner.class, args);
     }
 
 }
--- ultron/src/test/java/com/vision/ultron/MainRunnerTests.java
+++ ultron/src/test/java/com/vision/ultron/MainRunnerTests.java
@@ -1,10 +1,10 @@
-package com.vision.backend;
+package com.vision.ultron;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class MainRunnerTests {
 
     @Test
     void contextLoads() {
